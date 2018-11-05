6094e189d1b6965a98675a70799d94986e9ae82c
Barry Evans <barryevans80@gmail.com>
2018-11-05 23:07:44 +0000

    Exercise 5-1

--- pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise5Test.java
+++ pet-kata/src/test/java/org/eclipse/collections/petkata/Exercise5Test.java
@@ -10,14 +10,15 @@
 
 package org.eclipse.collections.petkata;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+import java.util.IntSummaryStatistics;
 import java.util.Set;
 
+import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.partition.list.PartitionMutableList;
+import org.eclipse.collections.api.set.primitive.IntSet;
 import org.eclipse.collections.api.set.primitive.MutableIntSet;
-import org.eclipse.collections.impl.factory.Sets;
+import org.eclipse.collections.impl.factory.Lists;
 import org.eclipse.collections.impl.factory.primitive.IntSets;
 import org.eclipse.collections.impl.test.Verify;
 import org.junit.Assert;
@@ -28,7 +29,9 @@ public class Exercise5Test extends PetDomainForKata
     @Test
     public void partitionPetAndNonPetPeople()
     {
-        PartitionMutableList<Person> partitionMutableList = null;
+        PartitionMutableList<Person> partitionMutableList = this.people
+                .partition(Person::isPetPerson);
+
         Verify.assertSize(7, partitionMutableList.getSelected());
         Verify.assertSize(1, partitionMutableList.getRejected());
     }
@@ -36,7 +39,10 @@ public class Exercise5Test extends PetDomainForKata
     @Test
     public void getOldestPet()
     {
-        Pet oldestPet = null;
+        Pet oldestPet = this.people
+                .flatCollect(Person::getPets)
+                .maxBy(Pet::getAge);
+
         Assert.assertEquals(PetType.DOG, oldestPet.getType());
         Assert.assertEquals(4, oldestPet.getAge());
     }
@@ -44,7 +50,14 @@ public class Exercise5Test extends PetDomainForKata
     @Test
     public void getAveragePetAge()
     {
-        double averagePetAge = 0;
+        IntSummaryStatistics stats = new IntSummaryStatistics();
+        this.people
+                .flatCollect(Person::getPets)
+                .collectInt(Pet::getAge)
+                .forEach(stats::accept);
+
+        double averagePetAge = stats.getAverage();
+
         Assert.assertEquals(1.8888888888888888, averagePetAge, 0.00001);
     }
 
@@ -53,6 +66,8 @@ public class Exercise5Test extends PetDomainForKata
     {
         // Hint: Use petAges as a target collection
         MutableIntSet petAges = IntSets.mutable.with(5);
+        IntSets.mutable.with(1, 2, 3, 4).collectInt(i -> i, petAges);
+
         Assert.assertEquals(IntSets.mutable.with(1, 2, 3, 4, 5), petAges);
     }
 
@@ -60,7 +75,7 @@ public class Exercise5Test extends PetDomainForKata
     public void refactorToEclipseCollections()
     {
         // Replace List and ArrayList with Eclipse Collections types
-        List<Person> people = new ArrayList<>();
+        MutableList<Person> people = Lists.mutable.empty();
         people.add(new Person("Mary", "Smith").addPet(PetType.CAT, "Tabby", 2));
         people.add(new Person("Bob", "Smith")
                 .addPet(PetType.CAT, "Dolly", 3)
@@ -75,18 +90,11 @@ public class Exercise5Test extends PetDomainForKata
         people.add(new Person("John", "Doe"));
 
         // Replace Set and HashSet with Eclipse Collections types
-        Set<Integer> petAges = new HashSet<>();
-        for (Person person : people)
-        {
-            for (Pet pet : person.getPets())
-            {
-                petAges.add(pet.getAge());
-            }
-        }
+        IntSet petAges = people
+                .flatCollect(Person::getPets)
+                .collectInt(Pet::getAge)
+                .toSet();
 
-        //extra bonus - convert to a primitive collection
-        Assert.assertEquals("Extra Credit - convert to a primitive collection", Sets.mutable.with(1, 2, 3, 4), petAges);
-
-        Assert.fail("Refactor to Eclipse Collections");
+        Assert.assertEquals("Extra Credit - convert to a primitive collection", IntSets.mutable.with(1, 2, 3, 4), petAges);
     }
 }
